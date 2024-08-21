99cd7a3ae7b1dc74f3331ba4473b12b76f840911
Keli Deng <673188101@qq.com>
2024-08-21 11:40:32 +0800

    Update rgb+hsi_denoise.sh

--- run/rgb+hsi_denoise.sh
+++ run/rgb+hsi_denoise.sh
@@ -3,6 +3,16 @@ save_name="RGB+HSI_DM4"
 dataset="cave"
 cuda=1
 
+# base_samples: path to dataset
+# save_dir: path to save the result
+# model_config: path to HSI DM config
+# in_channels: number of bands
+# range_t: perform unconditional denoising in the last range_t steps
+# num_samples: set to the size of dataset
+# rgb_model_config: path to RGB DM config
+# l1: index for gradient
+# l2: fusion weight of RGB DM
+
 CUDA_VISIBLE_DEVICES=$cuda python ../scripts/hsi_denoise.py \
  --base_samples /home/root/dataset/$dataset/$noise_model/ \
  --save_dir ../results/new_hsi/$dataset/$noise_model/$save_name/ \
