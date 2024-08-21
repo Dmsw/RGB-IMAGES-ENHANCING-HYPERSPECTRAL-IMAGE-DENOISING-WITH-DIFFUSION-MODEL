0b6fbf5d33df13fa0f9ff33af5bde5ddb57877a8
Keli Deng <673188101@qq.com>
2024-08-21 11:41:23 +0800

    Update train_hsi.sh

--- run/train_hsi.sh
+++ run/train_hsi.sh
@@ -2,6 +2,6 @@ CUDA_VISIBLE_DEVICES=0 python ../scripts/hsi_train.py \
     --data_dir "../../dataset/cave/randga75/" \
     --batch_size 4 \
     --save_interval 10000 \
-    --model_config ../config/model_config256.yaml \
+    --model_config ../config/model_config.yaml \
     --lr_anneal_steps 30000 \
     --save_dir ../models/hsi256_new2/
