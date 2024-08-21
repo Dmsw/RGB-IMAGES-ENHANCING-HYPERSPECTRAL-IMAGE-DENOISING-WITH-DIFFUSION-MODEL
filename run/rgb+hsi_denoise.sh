bd46b014894e95cc41fa02159052b0cb3da7ed48
Keli Deng <673188101@qq.com>
2024-08-21 11:11:11 +0800

    correct rgb+hsi_denoise.sh
    correct the setting of rgb_model_config and model_config

--- run/rgb+hsi_denoise.sh
+++ run/rgb+hsi_denoise.sh
@@ -6,10 +6,10 @@ cuda=1
 CUDA_VISIBLE_DEVICES=$cuda python ../scripts/hsi_denoise.py \
  --base_samples /home/root/dataset/$dataset/$noise_model/ \
  --save_dir ../results/new_hsi/$dataset/$noise_model/$save_name/ \
- --model_config ../models/hsi256/model_config.yaml \
+ --model_config ../config/model_config.yaml \
  --in_channels 31 \
  --range_t 0 \
  --num_samples 3 \
- --rgb_model_config ../models/DM256/model_config.yaml \
+ --rgb_model_config ../config/model_config_rgb.yaml \
  --l1 2 \
  --l2 0.52
